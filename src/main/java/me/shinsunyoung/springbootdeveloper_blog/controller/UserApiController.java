00fda1b5de338774b9631c5f30577e1d408ce4ea
shangcom <dndbs91@gmail.com>
2024-12-06 16:39:42 +0900

    스프링부트3 백엔드 개발자 되기
    레벨2 스프링 부트 3로 블로그 제대로 만들기
    
    8장 PROJECT 스프링 시큐리티로 로그인/로그아웃, 회원 가입 구현하기
    
    p258 뷰 작성하기까지 완료.
    csrf, HttpSequrity, SecurityFilterChain 등 정리중.

--- src/main/java/me/shinsunyoung/springbootdeveloper_blog/controller/UserApiController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper_blog/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.shinsunyoung.springbootdeveloper_blog.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper_blog.dto.AddUserRequest;
 import me.shinsunyoung.springbootdeveloper_blog.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +20,14 @@ public class UserApiController {
     @PostMapping("/user")
     public String signUp(AddUserRequest request) {
         userService.save(request); // 회원 갑입 메서드 호출
+        return "redirect:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String signUp(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+
         return "redirect:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
     }
 
