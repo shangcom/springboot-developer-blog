bf40bdcc596af4d8fbe16d531e770877ffe32d60
shangcom <dndbs91@gmail.com>
2024-12-13 16:23:46 +0900

    스프링부트3 백엔드 개발자 되기
    레벨2 스프링 부트 3로 블로그 제대로 만들기
    
    9장 Project JWT로 로그인/로그아웃 구현하기
    
    리프레시 토큰 도메인 구현하기 완료
    
    p288 토큰 필터 구현하기
    
    코드 입력 완료
    
    분석해야함

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper_blog/config/TokenAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package me.shinsunyoung.springbootdeveloper_blog.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper_blog.config.jwt.TokenProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizatioHeader = request.getHeader(HEADER_AUTHORIZATION);
+        String token = getAccessToken(authorizatioHeader);
+        if (tokenProvider.validToken(token)) {
+            Authentication autentication = tokenProvider.getAutentication(token);
+            SecurityContextHolder.getContext().setAuthentication(autentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) {
+            return authorizationHeader.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+}
